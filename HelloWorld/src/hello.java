f539802504cae4c7425153843c56b2bcb23105e5
nin64 <Nubbannie.Saani@city.ac.uk>
2022-03-09 20:04:37 +0000

    changes

---
+++ HelloWorld/src/hello.java
@@ -0,0 +1,15 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Bye World");
+        System.out.println("push by meraz");
+        System.out.println("Another change by Nabil");
+        System.out.println("push by nubba");
+<<<<<<< HEAD
+        System.out.println(2+2);
+=======
+        System.out.println("another by adil");
+
+>>>>>>> 84c69f9b314807d2f6643390dc41ac27f756bd59
+    }
+}
