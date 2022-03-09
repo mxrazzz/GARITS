45068abefe331c05ace45dca77b9ce80884e3f9a
am124 <71776604+am124@users.noreply.github.com>
2022-03-09 20:05:16 +0000

    adil's change

--- HelloWorld/src/hello.java
+++ HelloWorld/src/hello.java
@@ -1,11 +1,5 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Bye World");
-        System.out.println("push by meraz");
-        System.out.println("Another change by Nabil");
-        System.out.println("push by nubba");
-        System.out.println("another by adil");
-
+        System.out.println("Hello World adil");
     }
 }
