f640396a995930ea4d6285769466a8a8659f6174
NabilTalukder <71780298+NabilTalukder@users.noreply.github.com>
2022-03-11 20:54:04 +0000

    Add files via upload

---
+++ GARITS/src/User.java
@@ -0,0 +1,29 @@
+public class User  {
+    private String role;
+    private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
