3f888a9adc2bfb3ec745a6274c24813f5fe6aa87
am124 <71776604+am124@users.noreply.github.com>
2022-03-15 17:11:11 +0000

    edit to nubbas

---
+++ untitled1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        LoginForm loginForm = new LoginForm(null);
+        //Login.LoginMethod();
+
+        System.out.println("adil");
+
+    }
+}
+
+
